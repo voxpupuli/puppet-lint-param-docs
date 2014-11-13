ac0522d472e4469bbcf9801fb2d8099608d0190e
Gael Chamoulaud <gchamoul@redhat.com>
2014-11-13 10:33:46 +0100

    Add Puppet Doc Style check
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- lib/puppet-lint/plugins/check_parameter_documentation.rb
+++ lib/puppet-lint/plugins/check_parameter_documentation.rb
@@ -7,7 +7,10 @@ PuppetLint.new_check(:parameter_documentation) do
       tokens[0..klass[:start]].reverse_each do |dtok|
         next if [:CLASS, :NEWLINE, :WHITESPACE, :INDENT].include?(dtok.type)
         if [:COMMENT, :MLCOMMENT, :SLASH_COMMENT].include?(dtok.type)
-          doc_params << $1 if dtok.value =~ /\A\s*\$([a-zA-Z0-9_]+)::/
+          if dtok.value =~ /\A\s*\[\*([a-zA-Z0-9_]+)\*\]/ or
+             dtok.value =~ /\A\s*\$([a-zA-Z0-9_]+)::/
+            doc_params << $1
+          end
         else
           break
         end
